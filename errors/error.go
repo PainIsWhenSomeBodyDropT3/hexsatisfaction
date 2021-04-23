2b408dd705b43890f4012e0167d26c600d958df3
JesusG2000 <fpfpfppfp@mail.ru>
2021-04-23 22:24:37 +0300

    dot fix

--- errors/error.go
+++ errors/error.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// DatabaseError represents errors related with database .
+// DatabaseError represents errors related with database.
 func DatabaseError(details string, err error) error {
 	return fmt.Errorf("%s : %v", details, err) // errors.Wrap(err, details)
 }
